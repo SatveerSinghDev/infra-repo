6d9188b243a14a5731d8ab28a0bf755c11a4a0dc
Satveer Singh <satveersinghcaworks@gmail.com>
2026-05-23 14:31:14 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "app_sg" {
+  name = "taskmanagerEC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5001
+    to_port     = 5001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "server" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+
+  key_name = "demo"
+
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id
+  ]
+
+  tags = {
+    Name = "docker-server"
+  }
+}
+
+output "server_ip" {
+  value = aws_instance.server.public_ip
+}
